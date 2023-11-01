860e87f8e0ffae683a5a075ffe9309185a05fcbd
Ahmed-Waseem77 <ahmedwaseemr@aucegypt.edu>
2023-11-01 23:33:31 +0200

    testing .gitignore

--- valuni_wd/src/App.js
+++ valuni_wd/src/App.js
@@ -1,11 +1,8 @@
 
 import './App.css';  
-//import {LoginPage} from './login/LoginPage.js';    
 import {devComponent} from './fdev.js';
 
-//import parse from 'html-react-parser';
 //import testFirebase from './firebase/config.js';
-/*hi*/  
 
 
 
--- valuni_wd/src/fdev.js
+++ valuni_wd/src/fdev.js
@@ -4,11 +4,8 @@ import {LoginPage} from './login/LoginPage.js';
 //
 
 
-//DISPLAY YOUR COMPONENTS HERE 
-
-
-
+//DISPLAY YOUR COMPONENTS HERE INSTEAD OF THE LOGIN PAGE 
 var devComponent = ( <LoginPage />);
-
+//
 
 export {devComponent};
