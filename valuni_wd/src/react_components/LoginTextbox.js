2926374c615f50941322d2ae912dc9bcd411ecdb
SamanodyJr <sarahelsamanody@aucegypt.edu>
2023-11-03 23:21:59 +0200

    updated git ignore removing nodemodules

---
+++ valuni_wd/src/react_components/LoginTextbox.js
@@ -0,0 +1,12 @@
+import './LoginTextBox.css';
+import React, {Component} from 'react';
+
+class LoginTextBox extends  React.Component{
+
+    render(){
+        <div classname = "txtbox" id="txtbox" >
+            <input type = 'text' >
+            </input>
+        </div>
+    }
+}
