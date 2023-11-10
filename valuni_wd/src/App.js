4d6cf6febcb10c804951158dd53f6b966c4a7761
elbarbary <96207250+elbarbary@users.noreply.github.com>
2023-11-10 16:19:53 +0200

    Database connection, SignIn, SignUp functionality

--- valuni_wd/src/App.js
+++ valuni_wd/src/App.js
@@ -1,19 +1,28 @@
-
-import './App.css';  
-import {devComponent} from './fdev.js';
-
-//import testFirebase from './firebase/config.js';
-
-
+import React from 'react';
+import './App.css';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import { testFirebase, signInWithEmailAndPasswordFunction, signUpWithEmailAndPassword } from './firebase/config';
 
 function App() {
-  return ( 
-    {...devComponent}
+  // Call the testFirebase function to test Firebase connectivity
+  testFirebase();
+
+  const handleSignIn = (email, password) => {
+    signInWithEmailAndPasswordFunction(email, password);
+  };
+
+  const handleSignUp = (email, password) => {
+    signUpWithEmailAndPassword(email, password);
+  };
+
+  return (
+    <div>
+      <h1>Your App Title</h1>
+      <SignIn onSignIn={handleSignIn} />
+      <SignUp onSignUp={handleSignUp} />
+    </div>
   );
-} 
-
-
-
-//testFirebase();
+}
 
 export default App;
--- valuni_wd/src/firebase/config.js
+++ valuni_wd/src/firebase/config.js
@@ -1,38 +1,104 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendEmailVerification } from 'firebase/auth';  // Add this line
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 // AW: 2021-10-10:  Firebase configuration is removed from this file for security reasons.
 const firebaseConfig = {
-  apiKey:             "",
-  authDomain:         "",
-  projectId:          "",
-  storageBucket:      "",
-  messagingSenderId:  "",
-  appId:              "",
-  measurementId:      ""
+  apiKey: "AIzaSyChFnWRgOgVa55H2oa-kDclPwFvVw7mJns",
+  authDomain: "val-uni.firebaseapp.com",
+  databaseURL: "https://val-uni-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "val-uni",
+  storageBucket: "val-uni.appspot.com",
+  messagingSenderId: "707606250442",
+  appId: "1:707606250442:web:d76900f3f63a1b396c0533",
+  measurementId: "G-Q670T2RE5C"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app); 
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
 
-
-// function to  Test connectivity to Firebase 
-
-function testFirebase() {
+// function to Test connectivity to Firebase
+export function testFirebase() {
   if (app) {
-    console.log("Firebase is initialized successfully");
+    console.log('Firebase is initialized successfully');
+
+    // Test database connectivity
+    const db = getFirestore(app);
+    const exampleCollection = collection(db, 'exampleCollection');
+
+    getDocs(exampleCollection)
+      .then((querySnapshot) => {
+        console.log('Successfully connected to the database');
+        // You can further process the data if needed
+        querySnapshot.forEach((doc) => {
+          console.log(doc.id, ' => ', doc.data());
+        });
+      })
+      .catch((error) => {
+        console.error('Error connecting to the database:', error);
+      });
   } else {
-    console.log("Firebase initialization failed");
+    console.log('Firebase initialization failed');
   }
-} 
+}
 
+// Function to sign in user with email and password
+export function signInWithEmailAndPasswordFunction(email, password) {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      console.log('User signed in:', user.uid);
+    })
+    .catch((error) => {
+      console.error('Error signing in:', error.message);
+    });
+}
 
+// Function to sign up user with email and password
+export async function signUpWithEmailAndPassword(email, password) {
+  // Check if the email has the allowed domain
+  if (!email.endsWith('@aucegypt.edu')) {
+    console.error('Invalid email domain. Please use an @aucegypt.edu email.');
+    return;
+  }
 
-export default testFirebase;
+  try {
+    // Create user with email and password
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
+
+    // Send verification email
+    await sendEmailVerification(user);
+
+    console.log('Verification email sent to', email);
+    console.log('Waiting for email verification...');
+
+    // Wait for email verification
+    await waitForEmailVerification(user);
+
+    console.log('Email verified successfully.');
+  } catch (error) {
+    console.error('Error signing up:', error.message);
+  }
+}
+
+// Function to wait for email verification
+async function waitForEmailVerification(user) {
+  return new Promise((resolve, reject) => {
+    const intervalId = setInterval(() => {
+      // Check if the email is verified
+      if (user.emailVerified) {
+        clearInterval(intervalId);
+        resolve();
+      }
+    }, 1000); // Check every second
+  });
+}
