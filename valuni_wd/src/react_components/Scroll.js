82dd83c5fb1bcf2989c4cf4ac016061b23731300
SamanodyJr <sarahelsamanody@aucegypt.edu>
2023-11-11 22:52:49 +0200

    merge

---
+++ valuni_wd/src/react_components/Scroll.js
@@ -0,0 +1,28 @@
+import './Scroll.css';
+import React, {Component} from 'react';
+
+class Scroll extends  React.Component{
+
+    render(){
+        return(
+            <div className='container'>
+                <div className='txt'>
+                    <input type= "text" />
+                    <input type= "text" />
+                    <input type= "text" />
+                    <input type= "text" />
+                    <input type= "text" />
+                    <input type= "text" />
+                    <input type= "text" />
+                    <input type= "text" />
+                    <input type= "text" />
+                    <input type= "text" />
+                    <input type= "text" />
+                    <input type= "text" />
+                    <input type= "text" />
+                </div>
+            </div>
+        )}
+}
+
+export {Scroll}
