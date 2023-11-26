c96246d6823b3750a474cd9ba90a2bcc0ed3e66a
Hussein-Heggi <hussein_heggi@aucegypt.edu>
2023-11-26 13:03:43 +0200

    finalised home page queries and updated the dump file

---
+++ valuni_wd/src/SQL/Homepage_Queries.sql
@@ -0,0 +1,16 @@
+
+SELECT C.CRN, C.Engaging, C.Support, C.ContentQuality, C.Difficulty, C.Grading, C.Workload, I.First_Name, I.Last_Name, I.Grading, I.TeachingStyle, I.Flexibility, I.Availability
+FROM Users U INNER JOIN Take T ON U.Email = T.User_Email
+INNER JOIN Courses C ON T.CRN = C.CRN 
+INNER JOIN Teach Te ON C.CRN = Te.CRN
+INNER JOIN Instructors I ON Te.Inst_ID = I.ID 
+WHERE U.Major = C.Major AND U.Email = "sarah.yassin@aucegypt.edu"
+Limit 5;
+
+-- 
+
+SELECT C.CRN, C.Engaging, C.Support, C.ContentQuality, C.Difficulty, C.Grading, C.Workload, I.First_Name, I.Last_Name, I.Grading, I.TeachingStyle, I.Flexibility, I.Availability  
+FROM Courses C INNER JOIN Take T ON T.CRN = C.CRN
+INNER JOIN Instructors I ON T.Inst_ID = I.ID 
+WHERE T.User_Email = "sarah.yassin@aucegypt.edu" 
+LIMIT 5;
