f17e8c08ab48ab96b509cc02fe2ad021993ce94c
Youssef_Elmahdy <yelmahdy@aucegypt.edu>
2023-11-26 05:39:05 +0200

    Search

--- valuni_wd/src/react_components/RatingsBoxes.js
+++ valuni_wd/src/react_components/RatingsBoxes.js
@@ -1,42 +1,104 @@
-// RatingsBoxesEL.jsx
-import React from 'react';
+// TopBar.js
+import React, { Component } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import logo from '../resources/VAL-STARS-LOGO-LIGHT.svg';
+import { Link } from 'react-router-dom';
+import './TopBar.css';
 import Ratings from '../resources/VAL-STAR_FULL.svg';
-import './RatingsBoxes.css';
 
-class RatingsBoxesEL extends React.Component {
-  render() {
-    const { Text, ProfLast, ProfFirst, Sem } = this.props;
+class TopBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+      suggestions: [],
+    };
+  }
 
-    // Text = "HellozHellozHellozHellozHellozHellozHellozHellozHellozHellozHelloz";
-    // ProfLast = "Cena";
-    // ProfFirst = "John";
-    // Sem = "2X' 2X'";
-    return (
-      <div className='flexRatings'>
-        <div className='RatingsBox' id='RatingsBox'>
-          <div className='RatingDetails' id='RatingDetails'>
-            <h1 className='Rater' id='Rater'>
-              Anonymous
-            </h1>
-            <div className='StarsContainer'>
-              <img src={Ratings} className='Stars' alt='Stars' />
-              <img src={Ratings} className='Stars' alt='Stars' />
-              <img src={Ratings} className='Stars' alt='Stars' />
-              <img src={Ratings} className='Stars' alt='Stars' />
-              <img src={Ratings} className='Stars' alt='Stars' />
-            </div>
-          </div>
-          <h2 className='ProfDetails' id='ProfDetails'>
-            Prof. {ProfLast}, {ProfFirst}
-          </h2>
-          <h2 className='Semester' id='Semester'>
-            SEM {Sem}
-          </h2>
-          <div className='RatingText' id='RatingText'>{Text}</div>
+  handleInputChange = (event) => {
+    const inputValue = event.target.value;
+    const suggestions = this.generateSuggestions(inputValue);
+
+    this.setState({
+      inputValue,
+      suggestions,
+    });
+  };
+
+  generateStars = () => {
+    return Array.from({ length: 5 }).map((_, starIndex) => (
+      <img key={starIndex} src={Ratings} className='Stars' alt='Stars' />
+    ));
+  };
+
+  generateSuggestions = (inputValue) => {
+    const predefinedSuggestions = ['CSCE XXXX', 'CSCE XXXX', 'CSCE XXXX'];
+
+    if (inputValue.trim() === '') {
+      // If the input is empty, clear suggestions
+      return [];
+    }
+
+    const filteredSuggestions = predefinedSuggestions.filter(
+      (suggestion) =>
+        suggestion.toLowerCase().includes(inputValue.toLowerCase())
+    );
+
+    return filteredSuggestions.map((suggestion, index) => (
+      <Link key={index} to={`/page/${suggestion.toLowerCase()}`} className="dropdown-item">
+        <div className="suggestion-content">
+          <span>
+            {suggestion}
+            {this.generateStars()}
+          </span>
         </div>
+      </Link>
+    ));
+  };
+
+  handleButtonClick = () => {
+    console.log('Button clicked!');
+    // Add any additional logic you want to perform when the button is clicked
+  };
+
+  render() {
+    const { inputValue, suggestions } = this.state;
+
+    // Add a class to the body when the dropdown is active
+    const isDropdownActive = suggestions.length > 0;
+    document.body.classList.toggle('blur-background', isDropdownActive);
+
+    return (
+      <div className="Topbar-container">
+        <div className="Topbar" id="Topbar">
+          <object type="image/svg+xml" data={logo} className="App-logo" alt="logo"></object>
+          <div className="SearchBar" id="SearchBar">
+            <input
+              className="TxtBar"
+              type="text"
+              value={inputValue}
+              onChange={this.handleInputChange}
+              placeholder=""
+            />
+            <button
+              type="submit"
+              className="btnSearch"
+              onClick={this.handleButtonClick}
+            >
+              <FaSearch />
+            </button>
+            {suggestions.length > 0 && (
+              <div className="dropdown-menu">
+                {suggestions}
+              </div>
+            )}
+          </div>
+          <div className="label">AUC</div>
+        </div>
+        <div className="inside-corner"> </div>
       </div>
     );
   }
 }
 
-export { RatingsBoxesEL };
+export { TopBar };
